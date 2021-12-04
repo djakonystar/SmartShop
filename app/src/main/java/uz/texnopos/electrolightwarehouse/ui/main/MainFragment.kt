3973b24c80f14b3785ad4e2247854aae0e1782c9
Sharapat Kalabaev <kalabaevshj@gmail.com>
2021-12-05 02:23:44 +0500

    design is ready

---
+++ app/src/main/java/uz/texnopos/electrolightwarehouse/ui/main/MainFragment.kt
@@ -0,0 +1,7 @@
+package uz.texnopos.electrolightwarehouse.ui.main
+
+import androidx.fragment.app.Fragment
+import uz.texnopos.electrolightwarehouse.R
+
+class MainFragment : Fragment(R.layout.fragment_main) {
+}
