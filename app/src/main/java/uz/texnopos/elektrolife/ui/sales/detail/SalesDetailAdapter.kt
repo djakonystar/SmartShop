7e128641f48da49bdf904eaf450b72b4fd7eb36a
Jaksibay Khakimov <djakonystar@MacBook-Pro-Jaksibay.local>
2022-06-21 12:28:48 +0500

    Added productImage in SalesDetail and Warehouse

--- app/src/main/java/uz/texnopos/elektrolife/ui/sales/detail/SalesDetailAdapter.kt
+++ app/src/main/java/uz/texnopos/elektrolife/ui/sales/detail/SalesDetailAdapter.kt
@@ -2,6 +2,8 @@ package uz.texnopos.elektrolife.ui.sales.detail
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.stfalcon.imageviewer.StfalconImageViewer
 import uz.texnopos.elektrolife.R
 import uz.texnopos.elektrolife.core.BaseAdapter
 import uz.texnopos.elektrolife.core.extensions.*
@@ -18,23 +20,39 @@ class SalesDetailAdapter(private val settings: Settings) :
             binding.apply {
                 tvProductName.text = order.productName
                 tvBrand.text = order.brand
-                tvCount.text = itemView.context?.getString(
-                    R.string.price_text,
-                    order.count.checkModule.toSumFormat,
-                    order.unitId.unitConverter(tvCount.context)
-                )
-                tvCost.text = itemView.context?.getString(
-                    R.string.price_text,
-                    order.price.checkModule.toSumFormat,
-                    settings.currency
-                )
-
                 val total = order.count * order.price
-                tvTotalPrice.text = itemView.context?.getString(
-                    R.string.price_text,
+                tvTotalPrice.text = itemView.context.getString(
+                    R.string.sales_detail_sum_text,
+                    order.count.checkModule.toSumFormat,
+                    order.unitId.unitConverter(tvTotalPrice.context),
+                    order.price.checkModule.toSumFormat,
+                    settings.currency,
                     total.checkModule.toSumFormat,
                     settings.currency
                 )
+
+                if (order.image != null) {
+                    Glide.with(ivProduct)
+                        .load(order.image)
+                        .placeholder(R.drawable.image_placeholder)
+                        .into(ivProduct)
+                } else {
+                    ivProduct.setImageResource(R.drawable.image_placeholder)
+                }
+
+                ivProduct.onClick {
+                    order.image?.let { imageUrl ->
+                        StfalconImageViewer.Builder(itemView.context, arrayOf(imageUrl)) { view, url ->
+                            Glide.with(itemView.context)
+                                .load(url)
+                                .placeholder(R.drawable.image_placeholder)
+                                .into(view)
+                        }
+                            .allowSwipeToDismiss(true)
+                            .withTransitionFrom(ivProduct)
+                            .show()
+                    }
+                }
 
                 itemView.isEnabled = order.count != 0.0
                 root.onClick {
